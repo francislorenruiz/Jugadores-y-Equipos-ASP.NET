3ca4503c746d8fe15d26edab9608bf88f99cfd13
Francisco Lorente <francislorenruiz@outlook.es>
2020-04-14 12:41:34 +0200

    Añadido servicio de log

---
+++ ASP.NETJugadoresyEquipos/Services/Log/ILog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFinal.Services.Log
+{
+    public interface ILog
+    {
+        void EscribirEntrada(string mensaje);
+    }
+}
---
+++ ASP.NETJugadoresyEquipos/Services/Log/LogFichero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace EjercicioFinal.Services.Log
+{
+    public class LogFichero : ILog
+    {
+        private readonly string _path;
+
+        public LogFichero()
+        {
+            _path = AppDomain.CurrentDomain.BaseDirectory + "Log";
+            Directory.CreateDirectory(_path);
+        }
+
+        public void EscribirEntrada(string mensaje)
+        {
+            var fecha = DateTime.Now.ToString("dd/MM/yyyy");
+            var hora = DateTime.Now.ToString("HH:mm:ss");
+
+            using (var sw = new StreamWriter(_path + "/log.txt", true))
+            {
+                sw.WriteLine("[" + fecha + " " + hora + "] " + mensaje);
+            }
+        }
+    }
+}
